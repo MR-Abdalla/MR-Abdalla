6419ac6e27fa7cf0975feee731224c84644285dd
abdalla <AbdallaEL5647@gmail.com>
2025-07-09 03:13:42 -0700

    t

--- login/script.js
+++ login/script.js
@@ -308,3 +308,16 @@ function closeAlert() {
     document.getElementById('credentials-alert').style.display = 'none';
 }
 
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const columns = document.querySelectorAll(".footer-column h4");
+    columns.forEach(column => {
+        column.addEventListener("click", function () {
+            let parent = this.parentElement;
+            parent.classList.toggle("open");
+        });
+    });
+});
+
